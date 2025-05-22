457c634b69eb211e3bada207abc21d6cccbe22d7
SantiagoVieiura <svieira1211@gmail.com>
2025-05-21 19:16:02 -0500

    Airport Controller Santiago Vieira2

--- src/core/controller/AirportController.java
+++ src/core/controller/AirportController.java
@@ -7,6 +7,8 @@ import java.time.LocalDate;
 
 public class AirportController {
     private Storage storage = new Storage();
+    
+    
     public void registerPassenger(long id, String firstname, String lastname, int day , int month ,int year, int phoneCode, long phone, String country) {
         long aux = id;
         int counter = 0;
@@ -15,7 +17,9 @@ public class AirportController {
 
             counter += 1;
             aux /= id;
+            
         }
+        valid = validateDate(day , month, year) ;
         for (Passenger passenger : storage.getPassengers()){
             if(passenger.getId() == id){
                 valid = false;
@@ -29,20 +33,54 @@ public class AirportController {
         if (id <= 0 || counter > 15) {
            valid = false;
         }
-        boolean validDate = true;
-        if(2025<year || year<1909){
-            validDate = false;
         
-         
-        }
-        if (month<1 || 12<month ){
-            validDate = false;
-        }
         
         
         
         LocalDate birthDate = LocalDate.of(year, month, day);
         Passenger passenger = new Passenger(id, firstname, lastname, birthDate, phoneCode, phone, country);
 
+    }
+    
+    public static boolean validateDate(int day, int month, int year) {
+        boolean validDate = true;
+
+        
+        if (year < 1910 || year > 2024) {
+            validDate = false;
+        }
+
+       
+        else if (month < 1 || month > 12) {
+            validDate = false;
+        }
+
+       
+        else if (day < 1 || day > 31) {
+            validDate = false;
+        }
+
+        
+        else {
+            if (month == 2) {
+                
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    if (day > 29) {
+                        validDate = false;
+                    }
+                } else {
+                    if (day > 28) {
+                        validDate = false;
+                    }
+                }
+            } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                if (day > 30) {
+                    validDate = false;
+                }
+            }
+            
+        }
+
+        return validDate;
     }
 }
